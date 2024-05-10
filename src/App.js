623acd8e3ae2b5e755e25c2f58be53b53d1c6c66
WatchJani <jankokondic84@gmail.com>
2024-05-11 01:49:45 +0200

    marker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import Map from "react-map-gl";
+import Map, {Marker} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "./index.css"
 
 
 function App() {
   return (
-      <Map  style={{width:"100%", height:"100vh"}}
-        mapboxAccessToken="pk.eyJ1IjoiamFua283MjYyIiwiYSI6ImNsdzEzMTg1YTA2cnkyanBoeW9xaTRpZ3QifQ.7vbQ1Xgo0cBUlpxFx8LveQ"
-        initialViewState={{
-          longitude: -100,
-          latitude: 40,
-          zoom: 3.5,
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-      >
-        {/* <GeolocateControl
+    <Map style={{ width: "100%", height: "100vh" }}
+      mapboxAccessToken="pk.eyJ1IjoiamFua283MjYyIiwiYSI6ImNsdzEzMTg1YTA2cnkyanBoeW9xaTRpZ3QifQ.7vbQ1Xgo0cBUlpxFx8LveQ"
+      initialViewState={{
+        longitude: -100,
+        latitude: 40,
+        zoom: 3.5,
+      }}
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+    >
+
+      <Marker longitude={-100} latitude={40} anchor="bottom" >
+        <img className="circle" src="./balon.jpg"/>
+          
+        
+      </Marker>
+      {/* <GeolocateControl
           positionOptions={{ enableHighAccuracy: true }}
           trackUserLocation={true}
         /> */}
-      </Map>
+    </Map>
   );
 }
 
