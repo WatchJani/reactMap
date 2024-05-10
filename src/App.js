5d70f00f32d16e08d3eb2ab2b7931615f68fb3a2
WatchJani <jankokondic84@gmail.com>
2024-05-11 01:54:11 +0200

    koper location

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "./index.css"
 
 
+//https://docs.mapbox.com/playground/geocoding/?search_text=koper&proximity=ip&access_token=pk.eyJ1IjoiamFua283MjYyIiwiYSI6ImNrenZqYjhsZzAzaHoybnM5dTAzb25zajcifQ.2wwRrsWDu3ALDhhXxq4MTA&searchType=forward
+
 function App() {
   return (
     <Map style={{ width: "100%", height: "100vh" }}
       mapboxAccessToken="pk.eyJ1IjoiamFua283MjYyIiwiYSI6ImNsdzEzMTg1YTA2cnkyanBoeW9xaTRpZ3QifQ.7vbQ1Xgo0cBUlpxFx8LveQ"
       initialViewState={{
-        longitude: -100,
-        latitude: 40,
+        longitude: 13.7304781,
+        latitude: 45.5479864,
         zoom: 3.5,
       }}
       mapStyle="mapbox://styles/mapbox/streets-v11"
     >
 
-      <Marker longitude={-100} latitude={40} anchor="bottom" >
+      <Marker longitude={13.7304781} latitude={45.5479864} anchor="bottom" >
         <img className="circle" src="./balon.jpg"/>
           
         
